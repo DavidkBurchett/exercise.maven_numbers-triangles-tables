355001d01c59899dd54bf4c3a2ad111a5520dfc6
David_Kyle_Burchett <davidkburchett@gmail.com>
2020-07-15 16:05:43 -0400

    finised TriangleUtilities

--- src/main/java/com/github/perschola/TriangleUtilities.java
+++ src/main/java/com/github/perschola/TriangleUtilities.java
@@ -3,18 +3,26 @@ package com.github.perschola;
 public class TriangleUtilities {
 
     public static String getTriangle(int numberOfRows) {
-        return null;
+        String result = "";
+        for (int i=0; i<numberOfRows - 1; i++){
+            result += getRow(i+1) + "\n";
+        }
+        return result;
     }
 
     public static String getRow(int numberOfStars) {
-        return null;
+        String result = "";
+        for (int i=0; i<numberOfStars; i++){
+            result += ("*");
+        }
+        return result;
     }
 
     public static String getSmallTriangle() {
-        return null;
+        return getTriangle(5);
     }
 
     public static String getLargeTriangle() {
-        return null;
+        return getTriangle(10);
     }
 }

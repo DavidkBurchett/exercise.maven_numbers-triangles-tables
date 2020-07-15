c4288976df14103c1c080fbbbcf4685f905d257b
David_Kyle_Burchett <davidkburchett@gmail.com>
2020-07-15 15:33:29 -0400

    finised NumberUtilities

--- src/main/java/com/github/perschola/NumberUtilities.java
+++ src/main/java/com/github/perschola/NumberUtilities.java
@@ -3,34 +3,80 @@ package com.github.perschola;
 
 public class NumberUtilities {
     public static String getEvenNumbers(int start, int stop) {
-        return null;
+        String result = "";
+        if (start % 2 == 1) {
+            for (int i = start; i < stop; i += 2) {
+                result += i;
+            }
+        } else {
+            for (int i = start + 1; i < stop; i += 2) {
+                result += i;
+            }
+        }
+        return result;
     }
 
 
     public static String getOddNumbers(int start, int stop) {
-        return null;
+            String result = "";
+            if (start % 2 == 0) {
+                for (int i = start; i < stop; i += 2) {
+                    result += i;
+                }
+            } else {
+                for (int i = start + 1; i < stop; i += 2) {
+                    result += i;
+                }
+            }
+            return result;
     }
 
 
     public static String getSquareNumbers(int start, int stop, int step) {
-        return null;
+        String result = "";
+        for (int i = start; i<stop; i+=step) {
+            result += i * i;
+
+        }
+        return result;
     }
 
-    public static String getRange(int start) {
-        return null;
+    public static String getRange(int stop) {
+        String result = "";
+        for (int i = 0; i < stop; i++) {
+            result += i;
+
+        }
+        return result;
     }
+
 
     public static String getRange(int start, int stop) {
-        return null;
+        String result = "";
+        for (int i = start; i<stop; i++) {
+             result += i;
+
+        }
+        return result;
     }
 
 
     public static String getRange(int start, int stop, int step) {
-        return null;
+        String result = "";
+        for (int i = start; i<stop; i+=step) {
+            result += i;
+
+        }
+        return result;
     }
 
 
     public static String getExponentiations(int start, int stop, int step, int exponent) {
-        return null;
+        String result = "";
+        for (int i = start; i<stop; i+=step) {
+            result += (int)Math.pow(i, exponent);
+
+        }
+        return result;
     }
 }

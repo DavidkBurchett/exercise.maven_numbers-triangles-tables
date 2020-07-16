44a4950d41470a1d22661c4de9f267631ed07014
David_Kyle_Burchett <davidkburchett@gmail.com>
2020-07-16 15:22:05 -0400

    finised TableUtilities

--- src/main/java/com/github/perschola/TableUtilities.java
+++ src/main/java/com/github/perschola/TableUtilities.java
@@ -2,14 +2,29 @@ package com.github.perschola;
 
 public class TableUtilities {
     public static String getSmallMultiplicationTable() {
-        return null;
+        return getMultiplicationTable(5);
     }
 
     public static String getLargeMultiplicationTable() {
-        return null;
+        return getMultiplicationTable(10);
     }
 
     public static String getMultiplicationTable(int tableSize) {
-        return null;
+        String result = "";
+        for (int i=1; i<=tableSize; i++){
+            int count = i;
+            for (int j=1; j<=tableSize; j++){
+                if (count * j <10){
+                    result += "  " + count * j + " |";
+                } else if(count * j < 100){
+                    result += " " + count * j + " |";
+                } else {
+                    result += count * j + " |";
+                }
+            }
+            result += "\n";
+        }
+        return result;
     }
+
 }
